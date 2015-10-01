c64eeba4fd4ef0735998d93c04f8fd031c15c374
Rick Hambrook <rick@thespaceinbetween.co.nz>
2015-10-02 10:15:36 +1300

    feat(tests): add test for magic collisions

---
+++ tests/magicPrefixTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Tests for PHPUnit
+ *
+ * @author     Rick Hambrook <rick@rickhambrook.com>
+ * @copyright  2015 Rick Hambrook
+ * @license    https://www.gnu.org/licenses/gpl.txt  GNU General Public License v3
+ */
+
+require_once(implode(DIRECTORY_SEPARATOR, [__DIR__, "..", "src", "Nest.php"]));
+
+use \Hambrook\Nest as Nest;
+
+class magicPrefixTest extends PHPUnit_Framework_TestCase {
+
+	public function testSetMagic() {
+		$Nest = new Nest([]);
+		// Valid, 1 level)
+		$Nest->__data = "bar";
+		$this->assertEquals("bar",     $Nest->__data);
+		$this->assertEquals("bar",     $Nest->data);
+	}
+
+	public function testSetNestedMagic() {
+		$Nest = new Nest([]);
+		// Invalid, 1 level
+		$Nest->__plus__minus = "four";
+		$this->assertEquals("four",    $Nest->__plus__minus);
+	}
+
+}
