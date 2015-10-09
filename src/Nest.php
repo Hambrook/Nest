51d803533f879597397645e26a673c4bd96ffd8a
Rick Hambrook <rick@thespaceinbetween.co.nz>
2015-10-09 22:01:18 +1300

    feat(core): version bump

--- src/Nest.php
+++ src/Nest.php
@@ -9,7 +9,7 @@ namespace Hambrook;
  *
  * @package    Nest
  *
- * @version    1.2.2
+ * @version    1.2.3
  *
  * @author     Rick Hambrook <rick@rickhambrook.com>
  * @copyright  2015 Rick Hambrook
